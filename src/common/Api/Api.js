fc1d5ec32a2871b6865ad96118adb482974ec1f1
Diego Marcillo <dfmarcillop@gmail.com>
2022-06-10 14:40:48 -0500

    refactor: added fonts and api functions

---
+++ src/common/Api/Api.js
@@ -0,0 +1,129 @@
+import { apiUrl } from "../Config/Environments";
+import { Token } from "../Storage/Token";
+//import { store } from "../../index";
+//import { auth as authActions } from "../../services/Auth/AuthActions";
+
+export class Api {
+  post(url, data, header) {
+    if (!navigator.onLine) return { error: "DISCONNECT" };
+
+    let isFormData = data instanceof FormData;
+
+    return fetch(`${apiUrl}${url}`, {
+      method: "POST",
+      headers: header
+        ? header
+        : isFormData
+        ? { Authorization: `Bearer ${Token.getToken()}` }
+        : {
+            Accept: isFormData ? "" : "application/json",
+            "Content-type": isFormData ? "" : "application/json",
+            Authorization: `Bearer ${Token.getToken()}`,
+          },
+      body: isFormData ? data : JSON.stringify(data),
+    })
+      .then(async (response) => {
+        if (response.status === 401) {
+          //store.dispatch(authActions.logout());
+          return response;
+        }
+        response.payload = await response.json();
+        return response;
+      })
+      .catch((err) => err);
+  }
+
+  put(url, data, header) {
+    if (!navigator.onLine) return { error: "DISCONNECT" };
+
+    let isFormData = data instanceof FormData;
+
+    return fetch(`${apiUrl}${url}`, {
+      method: "PUT",
+      headers: header
+        ? header
+        : isFormData
+        ? { Authorization: `Bearer ${Token.getToken()}` }
+        : {
+            Accept: isFormData ? "" : "application/json",
+            "Content-type": isFormData ? "" : "application/json",
+            Authorization: `Bearer ${Token.getToken()}`,
+          },
+      body: isFormData ? data : JSON.stringify(data),
+    })
+      .then(async (response) => {
+        if (response.status === 401) {
+          //store.dispatch(authActions.logout());
+          return response;
+        }
+        response.payload = await response.json();
+        return response;
+      })
+      .catch((err) => err);
+  }
+
+  delete(url, id) {
+    if (!navigator.onLine) return { error: "DISCONNECT" };
+
+    url = new URL(`${apiUrl}${url}/${id}`);
+    return fetch(url, {
+      method: "DELETE",
+      headers: new Headers({
+        Authorization: `Bearer ${Token.getToken()}`,
+      }),
+    })
+      .then(async (response) => {
+        if (response.status === 401) {
+          //store.dispatch(authActions.logout());
+          return response;
+        }
+        response.payload = await response.json();
+        return response;
+      })
+      .catch((err) => err);
+  }
+
+  get(url, params) {
+    if (!navigator.onLine) return { error: "DISCONNECT" };
+
+    url = new URL(`${apiUrl}${url}`);
+    if (params)
+      Object.keys(params).forEach((key) =>
+        url.searchParams.append(key, params[key])
+      );
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${Token.getToken()}`,
+      },
+    })
+      .then(async (response) => {
+        if (response.status === 401) {
+          //store.dispatch(authActions.logout());
+          return response;
+        }
+        response.payload = await response.json();
+        return response;
+      })
+      .catch((err) => err);
+  }
+
+  convertFormData(data) {
+    let dataBody = new FormData();
+    Object.keys(data).forEach((key) => {
+      if (!Array.isArray(data[key])) {
+        const isFile = data[key] && data[key].size;
+        const isJson = typeof data[key] === "object";
+        dataBody.append(
+          key,
+          isFile || !isJson ? data[key] : JSON.stringify(data[key])
+        );
+      } else {
+        dataBody.append(key, JSON.stringify(data[key]));
+      }
+    });
+    return dataBody;
+  }
+}
+
+export default new Api();
--- src/scenes/Auth/Login/Login.js
+++ src/scenes/Auth/Login/Login.js
@@ -1,37 +1,48 @@
 import React from "react";
 import { Form, Button, Input } from "antd";
-import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons"; 
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 import { InputCustom } from "../../../components/atoms/InputCustom/InputCustom";
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-</style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+</style>;
 
 export const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="login">
       <div>
         <Form>
-        <h1 className="title-one">Iniciar sesion</h1>
+          <h1 className="title-one">Iniciar sesion</h1>
           <Form.Item>
             <label className="title-two">Correo*</label>
-            <InputCustom  placeholder="example@tucorreo.com"/>
+            <InputCustom placeholder="example@tucorreo.com" />
           </Form.Item>
           <Form.Item>
             <label className="title-two">Contraseña*</label>
-            <Input.Password 
-            placeholder="********" 
-            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            <Input.Password
+              placeholder="********"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
             />
           </Form.Item>
           <Form.Item>
-            <Button type="primary" block className="button-primary">Ingresar</Button>
+            <Button type="primary" block className="button-primary">
+              Ingresar
+            </Button>
           </Form.Item>
           <Form.Item className="footer">
             <label>No tienes una cuenta?</label>
-            <a href="../Signup">Registrarse</a>
-            <a href="">Olvidé mi contraseña</a>
+            <Button type="link" onClick={() => navigate("/signup")}>
+              Registrarse
+            </Button>
+            <Button type="link" onClick={() => navigate("/")}>
+              Olvidé mi contraseña
+            </Button>
           </Form.Item>
         </Form>
       </div>
