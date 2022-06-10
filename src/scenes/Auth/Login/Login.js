d94572449b6a34f56f47756a53d5592d47335dae
Rocio Quigua <rocioquigualopez@gmail.com>
2022-06-10 13:19:03 -0500

    feat:(first update)

--- src/scenes/Auth/Login/Login.js
+++ src/scenes/Auth/Login/Login.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { Form } from "antd";
+import { Form, Button, Input } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons"; 
 
 import { InputCustom } from "../../../components/atoms/InputCustom/InputCustom";
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+</style>
 
 export const Login = () => {
   return (
     <div className="login">
       <div>
         <Form>
+        <h1 className="title-one">Iniciar sesion</h1>
           <Form.Item>
-            <InputCustom />
+            <label className="title-two">Correo*</label>
+            <InputCustom  placeholder="example@tucorreo.com"/>
+          </Form.Item>
+          <Form.Item>
+            <label className="title-two">Contraseña*</label>
+            <Input.Password 
+            placeholder="********" 
+            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" block className="button-primary">Ingresar</Button>
+          </Form.Item>
+          <Form.Item className="footer">
+            <label>No tienes una cuenta?</label>
+            <a href="../Signup">Registrarse</a>
+            <a href="">Olvidé mi contraseña</a>
           </Form.Item>
         </Form>
       </div>
--- src/scenes/Auth/Signup/Signup.js
+++ src/scenes/Auth/Signup/Signup.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Form, Button, Input } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons"; 
+
+import { InputCustom } from "../../../components/atoms/InputCustom/InputCustom";
 
 export const Signup = () => {
   return (
-    <div>
-      <h1>
-        Signup
-      </h1>
+    <div className="signup">
+      <div>
+        <Form>
+          <h1>Registro</h1>
+          <h2>Información personal</h2>
+          <Form.Item>
+            <label className="name">Nombres*</label>
+            <InputCustom />
+          </Form.Item>
+          <Form.Item>
+            <label className="surname">Apellidos*</label>
+            <InputCustom />
+          </Form.Item>
+          <Form.Item>
+            <label>Celular*</label>
+            <Input maxLength={10} />
+          </Form.Item>
+          <Form.Item>
+            <label>Correo*</label>
+            <InputCustom  placeholder="example@tucorreo.com"/>
+          </Form.Item>
+          <Form.Item>
+            <label>Contraseña*</label>
+            <Input.Password
+            placeholder="********"
+            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" block >Siguiente</Button>
+          </Form.Item>
+          <Form.Item>
+            <label>Tienes una cuenta?</label>
+            <a href="../Login/Login.js">Ingresar</a>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
-}
+};
