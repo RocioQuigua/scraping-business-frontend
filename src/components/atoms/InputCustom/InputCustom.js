e3be2f6ae88f992ff052cd2cdc02e4b7a254ff29
Diego Marcillo <dfmarcillop@gmail.com>
2022-06-11 16:29:25 -0500

    refactor(InputCustom): added form item

--- src/components/atoms/InputCustom/InputCustom.js
+++ src/components/atoms/InputCustom/InputCustom.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Input } from "antd";
 
-export const InputCustom = ({ placeholder }) => {
-  return <Input className="input-custom" placeholder={placeholder} />;
+export const InputCustom = ({ placeholder, ...props }) => {
+  return (
+    <Input className="input-custom" placeholder={placeholder} {...props} />
+  );
 };
--- src/scenes/Auth/Login/Login.js
+++ src/scenes/Auth/Login/Login.js
@@ -1,28 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Form, Button, Input } from "antd";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 
 import { InputCustom } from "../../../components/atoms/InputCustom/InputCustom";
 
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-</style>;
-
 export const Login = () => {
+  const [form] = Form.useForm();
   const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    console.error("Values: ", values);
+  };
 
   return (
     <div className="login">
       <div>
-        <Form>
+        <Form onFinish={onFinish} form={form}>
           <h1 className="title-one">Iniciar sesion</h1>
-          <Form.Item>
-            <label className="title-two">Correo*</label>
+          <label className="title-two">Correo*</label>
+          <Form.Item name="email">
             <InputCustom placeholder="example@tucorreo.com" />
           </Form.Item>
-          <Form.Item>
-            <label className="title-two">Contraseña*</label>
+          <label className="title-two">Contraseña*</label>
+          <Form.Item name="password">
             <Input.Password
               placeholder="********"
               iconRender={(visible) =>
@@ -30,12 +31,20 @@ export const Login = () => {
               }
             />
           </Form.Item>
-          <Form.Item>
-            <Button type="primary" block className="button-primary">
-              Ingresar
-            </Button>
+          <Form.Item shouldUpdate noStyle>
+            {() => (
+              <Button
+                className="button-primary"
+                type="primary"
+                htmlType="submit"
+                disabled={!form.getFieldValue("email") || !form.getFieldValue('password')}
+                block
+              >
+                Ingresar
+              </Button>
+            )}
           </Form.Item>
-          <Form.Item className="footer">
+          <Form.Item className="footer" noStyle>
             <label>No tienes una cuenta?</label>
             <Button type="link" onClick={() => navigate("/signup")}>
               Registrarse
