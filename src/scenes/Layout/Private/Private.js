0115855ec61cef4e8341c4a3e0c04d97a78447c4
Rocio Quigua <rocioquigualopez@gmail.com>
2022-06-17 02:14:36 -0500

    feat(Router): created

--- src/scenes/Layout/Private/Private.js
+++ src/scenes/Layout/Private/Private.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Search } from "../../Search/Search";
+import { Search } from "../../../scenes/Search/Search";
+import { Header } from "../../../components/atoms/Header/Header";
+import { Profile } from "../../../scenes/Profile/Profile";
+import { Favorites } from "../../Favorites/Favorites";
+import { Business } from "../../Business/Business";
 
 export const Private = () => {
   return (
-    <>
     <Router>
-      <Routes>
-        <Route exact path="/search" element={<Search />} />
-        <Route exact path="/" element={<Search />} />
-        <Route path="*" element={<Search />} />
-      </Routes>
+      <div>
+        <Header />
+        <div>
+          <Routes>
+            <Route exact path="/search" element={<Search />} />
+            <Route exact path="/" element={<Search />} />
+            <Route exact path="/profile" element={<Profile />} />
+            <Route exact path="/favorites" element={<Favorites />} />
+            <Route exact path="/business" element={<Business />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
-    </>
   );
-}
+};
