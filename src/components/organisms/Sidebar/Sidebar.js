c9ca02ddee17eacbb58c99d97750ecf1d60fd5fd
Rocio Quigua <rocioquigualopez@gmail.com>
2022-06-17 02:13:18 -0500

    feat(Sidebar): update

--- src/components/organisms/Sidebar/Sidebar.js
+++ src/components/organisms/Sidebar/Sidebar.js
@@ -1,10 +1,50 @@
 import React from "react";
-import { Drawer, Button, Image } from "antd";
+import { Drawer, Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { auth as AuthActions } from "../../../services/Auth/AuthActions";
 
 export const Sidebar = ({ visible, setVisible }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const options = [
+    {
+      name: "Mi informacion",
+      description: "Gestion de tu informacion",
+      image: require("../../../assets/images/my-information.png"),
+      onClick: () => handleNavigate("/profile")
+    },
+    {
+      name: "Mi empresa",
+      description: "Gestion de tu empresa",
+      image: require("../../../assets/images/my-business.png"),
+      onClick: () => handleNavigate("/business")
+    },
+    {
+      name: "Mis favoritos",
+      description: "Gestion de tus favoritos",
+      image: require("../../../assets/images/my-favorite.png"),
+      onClick: () => handleNavigate("/favorites")
+    },
+    {
+      name: "Buscador",
+      description: "Realizar busqueda",
+      image: require("../../../assets/images/searcher.png"),
+      onClick: () => handleNavigate("/search")
+    },
+  ];
+
+  const handleNavigate = (route) => {
+    navigate(route);
+    onClose();
+  }
+
   const onClose = () => {
     setVisible(false);
   };
+
   return (
     <Drawer
       className="sidebar"
@@ -12,42 +52,39 @@ export const Sidebar = ({ visible, setVisible }) => {
       visible={visible}
       onClose={onClose}
     >
-      <div className="sidebar__options">
-        <Image
-          className="sidebar__img"
-          src="../../../img/user.png"
-          alt="user logo"
-        />
-        <div>
-          <h1>Juan Camilo Castillo</h1>
-          <p>Administrador</p>
+      <div className="sidebar__title">
+        <div className="sidebar__options">
+          <img
+            className="sidebar__img"
+            src= "../../../assets/images/user.png"
+            alt="user logo"
+          />
+          <div>
+            <h2>Juan Camilo Castillo</h2>
+            <p>Administrador</p>
+          </div>
         </div>
       </div>
-      <div className="sidebar__options">
-                <Image
-          className="sidebar__img"
-          src="../../../img/my-information.png"
-          alt="user logo"
-        />
-        <div className="sidebar__info">
-        <h2>Mi informacion</h2>
-        <p>Gestion de tu informacion</p>
-        </div>
-        </div>
-        <div className="sidebar__options">
-        <Image
-          className="sidebar__img"
-          src="../../../img/my-business.png"
-          alt="user logo"
-        />
-        <div className="sidebar__info">
-        <h2>Mi empresa</h2>
-        <p>Gestion de tu empresa</p>
-        </div>
-        </div>
+
+      <div className="sidebar__menu">
+        {options.map((option, index) => (
+          <div className="sidebar__options" key={index} onClick={option.onClick}>
+            <img
+              className="sidebar__img"
+              src={option.image}
+              alt="logo"
+            />
+            <div className="sidebar__info">
+              <h2>{option.name}</h2>
+              <p>{option.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
       <div className="sidebar__close">
-        <h2>v1.0</h2>
-        <Button className="sidebar__button" type="link">
+        <h2>v1.2</h2>
+        <Button className="sidebar__button" type="link" onClick={() => dispatch(AuthActions.logout())}>
           Cerrar sesion
         </Button>
       </div>
