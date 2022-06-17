d4e00d2874c89797fe0590fc3381faca44003cb4
Rocio Quigua <rocioquigualopez@gmail.com>
2022-06-17 02:11:51 -0500

    feat(Header): update

--- src/components/atoms/Header/Header.js
+++ src/components/atoms/Header/Header.js
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
-import { Button } from 'antd';
-import { MenuOutlined } from '@ant-design/icons'
-import { Sidebar } from '../../organisms/Sidebar/Sidebar';
+import React, { useState } from "react";
+import { Button } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import { Sidebar } from "../../organisms/Sidebar/Sidebar";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   const [visibleMenu, setVisibleMenu] = useState();
 
   return (
-    <div className='header'>
-      <div className='header__information'>
-        <h1>Search<strong>GO</strong></h1>
+    <div className="header">
+      <div className="header__information">
+        <h1 onClick={() => navigate("/")}>
+          Search<strong>GO</strong>
+        </h1>
         <h2>Juan Camilo</h2>
-        <Button 
-          className='header__btn-menu'
+        <Button
+          className="header__btn-menu"
           onClick={() => setVisibleMenu(true)}
         >
           <MenuOutlined />
         </Button>
+        <Sidebar visible={visibleMenu} setVisible={setVisibleMenu} />
+        {}
       </div>
-      <Sidebar visible={visibleMenu} setVisible={setVisibleMenu}/>
     </div>
   );
 };
