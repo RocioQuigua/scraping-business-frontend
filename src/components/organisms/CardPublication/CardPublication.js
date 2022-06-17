9f46eb1a0995ee21f882c5fa78ee0dc0a25d6561
Rocio Quigua <rocioquigualopez@gmail.com>
2022-06-17 02:12:54 -0500

    feat(CardPublication): update

--- src/components/organisms/CardPublication/CardPublication.js
+++ src/components/organisms/CardPublication/CardPublication.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "antd";
-import { StarOutlined } from "@ant-design/icons";
+import { StarFilled, StarOutlined } from "@ant-design/icons";
 
 export const CardPublication = ({
   title,
@@ -9,28 +9,36 @@ export const CardPublication = ({
   words,
   onClick,
   website,
+  isActive,
 }) => {
   return (
     <div className="card-publication">
       <div className="card-publication__card">
         <h1>{title}</h1>
-        <h2>{date}</h2>
-        <Button className="card-publication__start">
-          <StarOutlined />
-        </Button>
+        <div className="card-publication__card card-publication__card--group">
+          <h2>{date}</h2>
+          <button className="card-publication__start">
+            {isActive ? <StarFilled /> : <StarOutlined />}
+          </button>
+        </div>
       </div>
       <p>{description}</p>
       <div className="card-publication__website">
         <div className="card-publication__button">
           {words?.map((word, index) => (
-            <Button className="card-publication__button card-publication__button--results"
-              key={index}>
-              {word.name} 
-              (<strong>{word.count}</strong>)
+            <Button
+              className="card-publication__button card-publication__button--results"
+              key={index}
+            >
+              {word.name}(<strong>{word.count}</strong>)
             </Button>
           ))}
         </div>
-        <Button className="card-publication__button card-publication__button--sitie" type="link" onClick={() => window.open(website, "_blank")}>
+        <Button
+          className="card-publication__button card-publication__button--sitie"
+          type="link"
+          onClick={() => window.open(website, "_blank")}
+        >
           Visitar sitio
         </Button>
       </div>
