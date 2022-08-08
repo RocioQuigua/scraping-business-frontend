0fb6fd92d55e39ba16733a5d18c9eba68f765f3c
Diego Marcillo <dfmarcillop@gmail.com>
2022-08-08 16:11:50 -0500

    refactor(Header): add new icon

--- src/components/atoms/Header/Header.js
+++ src/components/atoms/Header/Header.js
@@ -15,9 +15,10 @@ export const Header = () => {
   return (
     <div className="header">
       <div className="header__information">
-        <h1 onClick={() => navigate("/")}>
-          Udlavite
-        </h1>
+        <div className="header__container-title">
+          <h1 onClick={() => navigate("/")}>Udlavite</h1>
+          <img src="logo.png"  alt="logo"/>
+        </div>
         <div className="header__information header__information--group">
           <h2 onClick={() => setVisibleMenu(true)}>{profile?.person?.name}</h2>
           <Button
